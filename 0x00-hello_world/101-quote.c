6c94b21227ec47652bc83ac1c2b2ecb3248ee770
Barroh <barroh2003@gmail.com>
2023-09-07 16:58:43 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - A program that prints a standard error
- * Return: 1 (Success)
+ * Return 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
 	
